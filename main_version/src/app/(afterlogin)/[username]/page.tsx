e049e97c38a599ab81f5d2f86c06ce03bbdf7b65
Dongkeun <hongdongk@naver.com>
2024-03-18 19:36:20 +0900

    explore, profile 페이지 생성

--- main_version/src/app/(afterlogin)/[username]/page.tsx
+++ main_version/src/app/(afterlogin)/[username]/page.tsx
@@ -2,6 +2,8 @@
 
 import style from './profile.module.css';
 import { signOut, useSession } from 'next-auth/react';
+import Post from '../_component/Post';
+import BackButton from '../_component/BackButton';
 
 type Props = {
   params: { username: string };
@@ -13,9 +15,29 @@ export default function Profile({ params }: Props) {
     return null;
   }
   return (
-    <>
-      <div>닉네임 : {me.user?.name}</div>
-      <div>/아이디: @{me.user?.email}</div>
-    </>
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{me.user?.name}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={me.user?.image} alt={me.user?.name} />
+        </div>
+        <div className={style.userName}>
+          <div>{me.user?.name}</div>
+          <div>@{me.user?.email}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
   );
 }
--- main_version/src/app/(afterlogin)/explore/page.tsx
+++ main_version/src/app/(afterlogin)/explore/page.tsx
@@ -1,3 +1,24 @@
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+import TrendSection from '../_component/TrendSection';
+import style from './explore.module.css';
+
 export default function Explore() {
-  return <div>탐색</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <TrendSection />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
